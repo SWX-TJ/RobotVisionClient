c3c3545fd5b8e9c2508fa57c5b3d92c49cc7b900
swxhss <893636802@qq.com>
2017-08-06 17:04:32 +0800

    增加了双边滤波器消除采集设备噪声

--- image_processthread.cpp
+++ image_processthread.cpp
@@ -111,7 +111,8 @@ void Image_processThread::run()
         {
             if(!AutoWhiteBalance)
             {
-                Mat frame,preset_frame,dst_frame,resize_frame;vector<Mat>imageBGR;
+
+                Mat frame,preset_frame,bilater_frame,dst_frame,resize_frame;vector<Mat>imageBGR;
                 m_leftCamera->operator >>(frame);
                 /**/
                 //TODO 图像处理区域
@@ -121,11 +122,13 @@ void Image_processThread::run()
                 imageBGR[2] = imageBGR[2]*R_Gen;
                 merge(imageBGR,frame);
                 preset_frame= contrastAndBrightSet(frame,Contrast_Gen,Bright_Gen);
+               //双边滤波器
+                bilateralFilter(preset_frame,bilater_frame,10,10*2,10/2);
                 /**/
                 /**********/
                 //TODO opencv to qt显示
 
-                dst_frame = preset_frame.clone();
+                dst_frame = bilater_frame.clone();
                 resize(dst_frame,resize_frame,Size(320,240));
                 left_frame = convertMatToQImage(resize_frame);
                 send_leftdispframe(left_frame);
@@ -133,15 +136,48 @@ void Image_processThread::run()
                 /**********/
                 //TODO 图像测试处理区域
 
-
-                /**********/
-                imshow("left_video",preset_frame);
+//               Mat bilater_frame,gray_frame;
+//             //  boxFilter(preset_frame,BoxFliter_frame,-1,Size(5,5),Point(1,0));
+//               bilateralFilter(preset_frame,bilater_frame,10,10*2,10/2);
+//               cvtColor(bilater_frame,gray_frame,COLOR_BGR2GRAY);
+//               int m = getOptimalDFTSize(gray_frame.rows);
+//               int n = getOptimalDFTSize(gray_frame.cols);
+//               Mat padded;
+//               copyMakeBorder(gray_frame,padded,0,m-gray_frame.rows,0,n-gray_frame.cols,BORDER_CONSTANT,Scalar::all(0));
+//               Mat planes[] = {Mat_<float>(padded),Mat::zeros(padded.size(),CV_32F)};
+//               Mat complexI;
+//               merge(planes,2,complexI);
+//               dft(complexI,complexI);
+//               split(complexI,planes);
+//               magnitude(planes[0],planes[1],planes[0]);
+//               Mat magnitudeImage = planes[0];
+//               magnitudeImage +=Scalar::all(1);
+//               log(magnitudeImage,magnitudeImage);
+//               magnitudeImage = magnitudeImage(Rect(0,0,magnitudeImage.cols & -2,magnitudeImage.rows & -2));
+//               int cx = magnitudeImage.cols/2;
+//               int cy = magnitudeImage.rows/2;
+//               Mat q0(magnitudeImage,Rect(0,0,cx,cy));
+//               Mat q1(magnitudeImage,Rect(cx,0,cx,cy));
+//               Mat q2(magnitudeImage,Rect(0,cy,cx,cy));
+//               Mat q3(magnitudeImage,Rect(cx,cy,cx,cy));
+//               Mat tmp;
+//               q0.copyTo(tmp);
+//               q3.copyTo(q0);
+//               tmp.copyTo(q3);
+//               q1.copyTo(tmp);
+//               q2.copyTo(q1);
+//               tmp.copyTo(q2);
+//               normalize(magnitudeImage,magnitudeImage,0,1,NORM_MINMAX);
+//               imshow("频谱幅值",magnitudeImage);
+               /**********/
+                imshow("left_video",bilater_frame);
+               // imshow("bilater_fliter",bilater_frame);
                 waitKey(30);
             }
             else
             {
                  AutoWhiteBalance = !AutoWhiteBalance;
-                Mat frame,preset_frame,dst_frame,resize_frame;vector<Mat>imageBGR;
+                 Mat frame,preset_frame,bilater_frame,dst_frame,resize_frame;vector<Mat>imageBGR;
                 m_leftCamera->operator >>(frame);
                 /**/
                 //TODO 图像处理区域
@@ -162,11 +198,13 @@ void Image_processThread::run()
                 imageBGR[2] = imageBGR[2]*KR;
                 merge(imageBGR,frame);
                  preset_frame= contrastAndBrightSet(frame,Contrast_Gen,Bright_Gen);
-                /**/
+                //双边滤波器
+                 bilateralFilter(preset_frame,bilater_frame,10,10*2,10/2);
+                 /**/
                 /**********/
                 //TODO opencv to qt显示
 
-                dst_frame =  preset_frame.clone();
+                dst_frame = bilater_frame.clone();
                 resize(dst_frame,resize_frame,Size(320,240));
                 left_frame = convertMatToQImage(resize_frame);
                 send_leftdispframe(left_frame);
@@ -176,7 +214,7 @@ void Image_processThread::run()
 
 
                 /**********/
-                imshow("left_video", preset_frame);
+                imshow("left_video", bilater_frame);
                 waitKey(30);
             }
         }
